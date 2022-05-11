3eba2265a04e209d6ead2bb456e02bad1868dcf0
mruehlemann <malte.ruehlemann@gmail.com>
2022-05-11 09:03:48 +0200

    update drep

---
+++ binning/multibin_07a_drep_fastANI99.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+import drep
+import drep.d_cluster.external
+import drep.d_cluster.utils
+import drep.d_cluster.compare_utils
+
+
+wd=drep.WorkDirectory.WorkDirectory(wd)
+Ndb = self.wd.get_db('Ndb')
+id="primary_cluster"
+
+Cdb = pd.DataFrame()
+c2ret = {}
+for name, ndb in Ndb.groupby(id):
+	cdb, cluster_ret = drep.d_cluster.cluster_utils.genome_hierarchical_clustering(ndb, cluster=name, comp_method="fastANI", cluster_method="average")
+	cdb[id] = name
+	Cdb = pd.concat([Cdb,cdb], ignore_index=True)
+	c2ret[name] = cluster_ret
+
+wd.store_db(Cdb, "Cdb_99")
